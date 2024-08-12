946d14ff6585a96e8801366e4f8b5a7cbd6fa49f
danielxDx3 <edaniel.morenog@gmail.com>
2024-08-12 15:52:31 -0500

    xs

--- saludo.js
+++ saludo.js
@@ -1,11 +1,31 @@
-saludar=function(){
-    let nombre =recuperarTexto("txtNombre");
-    let apellido=recuperarTexto("txtApellido");
-}
-recuperarTexto=function(idComponente){
+recuperarTexto = function(idComponente) {
     let componente;
     let valorIngresado;
     componente = document.getElementById(idComponente);
-    valorIngresado=componente.value;
+    valorIngresado = componente.value;
     return valorIngresado;
+}
+
+recuperarInt = function(idComponente) {
+    let valorCaja = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valorCaja, 10);
+    return valorEntero;
+}
+
+recuperarFloat = function(idComponente) {
+    let valorCaja = recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
+}
+
+saludar = function() {
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+    let edad = recuperarInt("txtEdad");
+    let estatura = recuperarFloat("txtEstatura");
+    
+    console.log("Nombre:", nombre);
+    console.log("Apellido:", apellido);
+    console.log("Edad:", edad);
+    console.log("Estatura:", estatura);
 }
